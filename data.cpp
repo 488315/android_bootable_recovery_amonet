f3a8ae2e9f912230a1862cd57619403a157dab3d
Joel Stein <js@funkturm.net>
2019-07-16 14:59:44 +0200

    Add build-flag to define default backup_list
    Change-Id: I3d18a0875150362fd63623860b9d6bd69d55557c
    Signed-off-by: Kai Jones <joneskai626@gmail.com>

--- data.cpp
+++ data.cpp
@@ -756,7 +756,12 @@ void DataManager::SetDefaultValues()
 #ifdef PRODUCT_USE_DYNAMIC_PARTITIONS
 	mPersist.SetValue("tw_backup_list", "/data;");
 #else
+#ifdef TW_DEFAULT_BACKUP_LIST
+	mPersist.SetValue("tw_backup_list", EXPAND(TW_DEFAULT_BACKUP_LIST));
+#else
 	mPersist.SetValue("tw_backup_list", "/system;/data;/boot;");
+#endif
+#endif
 #endif
 #endif
 	mConst.SetValue(TW_MIN_SYSTEM_VAR, TW_MIN_SYSTEM_SIZE);
